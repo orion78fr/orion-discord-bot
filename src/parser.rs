84c88e00220b17383714ddc594ccf5fcb94d5e74
orion78fr <orion78fr@gmail.com>
2019-02-26 00:23:35 +0100

    Test of pase

--- src/parser.rs
+++ src/parser.rs
@@ -12,3 +12,39 @@ pub fn parse_message(msg: &str) -> Result<Vec<&str>, pest::error::Error<Rule>> {
         .map(|pair| pair.as_str())
         .collect());
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::parser::parse_message;
+
+    #[test]
+    fn simple() {
+        assert_eq!(parse_message("a"), Ok(vec!("a")));
+    }
+
+    #[test]
+    fn simple_quoted() {
+        assert_eq!(parse_message("\"a\""), Ok(vec!("a")));
+    }
+
+    #[test]
+    fn two() {
+        assert_eq!(parse_message("a b"), Ok(vec!("a", "b")));
+    }
+
+    #[test]
+    fn two_quote_1() {
+        assert_eq!(parse_message("\"a\" b"), Ok(vec!("a", "b")));
+    }
+
+    #[test]
+    fn two_quote_2() {
+        assert_eq!(parse_message("a \"b\""), Ok(vec!("a", "b")));
+    }
+
+    #[test]
+    fn many() {
+        assert_eq!(parse_message("a bb \"c c\" ddd \"e\" f"),
+                   Ok(vec!("a", "bb", "c c", "ddd", "e", "f")));
+    }
+}
